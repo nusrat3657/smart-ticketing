3a36a810b8a520a5814c044d14acc8373558871a
nusrat3657 <nusratjahan3657r@gmail.com>
2024-02-19 17:00:57 +0600

    added total price function

--- js/index.js
+++ js/index.js
@@ -27,8 +27,40 @@ for(let seat of seatButtons){
 
             seatSub.innerText = subSeat;
 
-            const placeTicket = e.target;
-            console.log(e.target);
+            const placeTicket = e.target.parentNode.childNodes[3].innerText;
+            // const place = document.getElementById("seat-button");
+            // const placeTicket = document.querySelector("kbd", place).innerText;
+            // console.log(placeTicket);
+            
+            const price = parseFloat(document.querySelector("h5").innerText);
+            // console.log(price);
+
+            // const price = e.target
+            // console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes);
+
+            const selectedContainer = document.getElementById('selected-ticket-container');
+
+            const tr = document.createElement('tr');
+
+            const td = document.createElement('td');
+            td.innerText = placeTicket;
+            
+            const td2 = document.createElement('td');
+            td2.innerText = "AC_Business";
+            const td3 = document.createElement('td');
+            td3.innerText = price;
+
+            tr.appendChild(td);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            // console.log(tr);
+            selectedContainer.appendChild(tr);
+
+            // calculate price
+            totalPrice += price;
+            document.getElementById('total-price').innerText = totalPrice;
+
+            const grandPrice.innerText = totalPrice;
         }
         else{
             countSeat = countSeat;
