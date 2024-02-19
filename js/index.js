9931792f3c4bad3f9af7741e05eb7843ec2453af
nusrat3657 <nusratjahan3657r@gmail.com>
2024-02-19 06:34:06 +0600

    updated sub seat function

--- js/index.js
+++ js/index.js
@@ -1,29 +1,39 @@
 const seatButtons = document.getElementsByClassName('seat');
 let countSeat = 0;
-let seatFaka = 0;
+let seatSub = 40;
 let totalPrice = 0;
 let totalClick = 0;
 
 for(let seat of seatButtons){
-    seat.addEventListener('click', function(){
+    seat.addEventListener('click', function(e){
         
-        if (countSeat <= 4) {
-            countSeat += 1;
+        if (countSeat < 4) {
+            countSeat = countSeat + 1;
             seat.style.backgroundColor = "#1DD100";
             setInnerText('seat-count', countSeat);
-            let seatAdd = document.getElementById('seat-count');
+            const seatAdd = document.getElementById('seat-count');
 
-        const seatText = parseInt(seatAdd.innerText);
+            const seatAddText = parseInt(seatAdd.innerText);
         
 
-        seatAdd.innerText = countSeat;
+            seatAdd.innerText = countSeat;
 
-        const seatFaka = document.getElementById('faka-seat');
+            let subSeat = document.getElementById('sub-seat');
+
+            const seatSubText = parseInt(subSeat.innerText);
+            seatSub = seatSub - 1;
+            setInnerText('sub-seat', seatSub)
+            
+
+            seatSub.innerText = subSeat;
+
+            const placeTicket = e.target;
+            console.log(e.target);
         }
         else{
-            countSeat = countSeat
+            countSeat = countSeat;
         }
-        // seat.setAttributes('disabled', ' ');
+        
 
         
     })
@@ -32,11 +42,3 @@ function setInnerText(id, value) {
     const setText = document.getElementById(id);
     setText.innerText = value;
 }
-// function changeColor() {
-//     this.style.backgroundColor = 'green'; 
-//   }
-  
-//   const seats = document.getElementsByClassName('kbd');
-//   seats.forEach(function(seat) {
-//     seat.addEventListener('click', changeColor);
-//   });
